fb035c0e306f8e3261e8dfc340598a807fda5a1e
giuliacrr <giulia.corrado02@gmail.com>
2023-07-17 19:50:53 +0200

    prev Btn and click Thumnails in the making

--- js/main.js
+++ js/main.js
@@ -21,16 +21,88 @@ const images = [
     text: 'Boom boom. DOOM Eternal is now available now on Xbox One, PS4, PC, and Nintendo Switch.',
   }
 ];
-
-
+//In evidence image container
+const inEvid = document.querySelector(".evidence-container");
+//Thumbnail container
 const thumbs = document.querySelector(".thumbnail-container");
+//Image index 
+let imgIndex = 0; //Let's start with giving it 0
+
+console.log("Here is the array:")
 console.log(images);
+console.log("Here are the objects in the array:")
 
 for (let i = 0; i < images.length; i++) {
-  const games = images[i];
-  console.log(games);
-  thumbs.innerHTML = `<div class="thumbnails"></div>`;
+  console.log("#" + i + " " + "Game");
+  console.log("Image:");
+  console.log(images[i].image);
+  console.log("Title:");
+  console.log(images[i].title);
+  console.log("Description:");
+  console.log(images[i].text);
+  console.log("---")
 
+  let active = "";
 
+  if (i === 0) {
+    active = "active";
+  }
+  //Let's print the images in Evidence
+  inEvid.innerHTML += `
+        <div class="evidence-img ${active}">
+          <img class="game-img" src="${images[i].image}" alt="game img">
+        </div>
+        <div class="game-description p-2 ${active}">
+          <h3 id="img-title">${images[i].title}</h3>
+          <p id="img-descript">${images[i].text}</p>
+        </div>
+`
+  //Thumbnails
+  thumbs.innerHTML += `
+    <div class="thumbnails ${active}">
+      <img class="thumbnails-img" src="${images[i].image}" alt="game-img">
+    </div>
+  `
 }
 
+
+//Let's create now the events for the prev and next btns
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+//Next Button
+nextBtn.addEventListener("click", function () {
+  console.log("next");
+
+  //Img in Evidence
+  const evidenceImgs = document.querySelectorAll(".evidence-img");
+  //Thumbnails border
+  const thumbImgs = document.querySelectorAll(".thumbnails");
+  //Title and Text
+  const infos = document.querySelectorAll(".game-description");
+
+  //Let's remove the active class from the active image
+  evidenceImgs[imgIndex].classList.remove("active");
+  thumbImgs[imgIndex].classList.remove("active");
+  infos[imgIndex].classList.remove("active");
+  //and let's skin to the next img index
+  imgIndex++
+  //Let's put this contition that makes us return to 0 when the index is max
+  if (imgIndex > evidenceImgs.length - 1) {
+    imgIndex = 0;
+  }
+  if (imgIndex > thumbImgs.length - 1) {
+    imgIndex = 0;
+  }
+  if (imgIndex > infos.length - 1) {
+    imgIndex = 0;
+  }
+  //Let's assign the class active to the next active img-text-thumbnails border
+  evidenceImgs[imgIndex].classList.add("active");
+  thumbImgs[imgIndex].classList.add("active");
+  infos[imgIndex].classList.add("active");
+})
+
+//Prev Button
+prevBtn.addEventListener("click", function () {
+  console.log("prev");
+})
